ee6b974a809454c9b758499d227f536489cfdb13
mohammedaladhary <caladhary@gmail.com>
2023-06-18 11:30:00 +0400

    static instance

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace instance_static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("enter a number: ");
+            //int n = int.Parse(Console.ReadLine());
+
+            example1 anyName = new example1(5); //20
+            anyName.getDoubleSomeInteger();
+            Console.WriteLine(example1.someInteger);
+
+            example1 anyName2 = new example1(6); //200
+            anyName2.getDoubleSomeInteger();
+            Console.WriteLine(example1.someInteger);
+
+            anyName.getDoubleSomeInteger(); //2000
+            Console.WriteLine(example1.someInteger);
+        }
+    }
+}
---
+++ example1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instance_static
+{
+    internal class example1
+    {
+        public int numberOfCars;
+        public static int someInteger = 2;
+
+        public example1(int tyres)
+        {
+            numberOfCars = tyres;
+        }
+
+        public int getDoubleSomeInteger()
+        {
+            someInteger = someInteger * 10;
+            return numberOfCars * 2;
+        }
+
+        public static void getFloatSomeInteger()
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
